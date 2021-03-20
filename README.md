440491d88de0b7dcede810557e582675032f1b65
04burhanuddin <dev.burhanuddin@gmail.com>
2021-03-20 18:02:17 +0700

    update code

--- README.md
+++ README.md
@@ -1,12 +1,9 @@
 ## My Profile
-
 [![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)<br>
 dibuat dengan menggunakan
-
 - HTML5
 - Bootstrap V5.0
 - CSS3
 - js
 - link Demo : https://04burhanuddin.github.io/My-Portfolio/
-
 ## Finally Meskipun Sangat Sederhana karna tidak ada tambahan JS
--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 //pertama kita buat event dimana saat tombol di click
 // $ adalah jquery
-$('.page-scroll').on('click', function (e) {
-
+$(".page-scroll").on("click", function (e) {
     // console.log('ok'); untuk menguji sudah terhubung atau belum
 
     // ambil isi href
-    var tujuan = $(this).attr('href');
+    var tujuan = $(this).attr("href");
 
     // tangkap element
     var elemenTujuan = $(tujuan);
@@ -14,15 +13,14 @@ $('.page-scroll').on('click', function (e) {
     // jarak ini agar tidak ketutupan setiap kali saya click menu di nav nya, langsung di tulis di
     // .top - 60 juga bisa.
     $jarak = 60;
-    $('html,body').stop().animate({
-        'scrollTop': elemenTujuan.offset().top - $jarak
+    $("html,body").stop().animate({
+        scrollTop: elemenTujuan.offset().top - $jarak,
         // nilainya bisa kita ubah sesuka hati dan efek nya bisa kita ubah dengan kesukaan kita contoh saya
-        // menggunakan yang default saja yaitu swing, jka ingin lebih banyak 
+        // menggunakan yang default saja yaitu swing, jka ingin lebih banyak
         //tinggal mengujungu halaman https://gsgd.co.uk/sandbox/jquery/easing/ lalu
         // ke bagian example untuk menentukan pilihan efek.
         // 1000 adalah waktu dalam satuan ms
-    }, 1000, 'swing');
-
+    }, 1000, "swing");
     // mematikan fasilitas a href
     e.preventDefault();
 });
